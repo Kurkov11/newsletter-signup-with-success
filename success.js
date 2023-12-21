1537fa2526e6856d2b52fb6ccafa4c400e367d34
Kurkov11 <kurkovsw@icloud.com>
2023-12-21 17:23:24 +0100

    Add shrek

--- success.js
+++ success.js
@@ -1,1 +1,8 @@
 document.getElementById('entered-email').innerHTML = localStorage.getItem('email-value');
+document.getElementById('success-wrapper').style.backgroundImage = 'none';
+document.getElementById('dismiss-btn').onclick = function(){
+
+    document.getElementById('success-wrapper').style.backgroundImage = 'url(assets/images/shrek-shadow.png)';
+    document.getElementById('success-wrapper').style.backgroundSize = 'cover'
+    window.location = 'index.html';
+}
