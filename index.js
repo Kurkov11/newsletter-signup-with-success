674cdff4c59d486cf07a1ca979588ba53eafa65c
Kurkov11 <kurkovsw@icloud.com>
2023-12-21 15:50:42 +0100

    Add function validating emails

---
+++ index.js
@@ -0,0 +1,10 @@
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+document.getElementById('subscribe-btn').onclick = function(){
+    if(! isEmail(document.getElementById('email-input').value)){
+
+    }else{
+        
+    }
+}
