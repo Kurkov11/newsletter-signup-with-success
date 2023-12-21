99be32d47656328df88c44f36e37dd68c8cda029
Kurkov11 <kurkovsw@icloud.com>
2023-12-21 16:12:04 +0100

    Show error msg, style email-input when email wrong

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ function isEmail(email) {
 }
 document.getElementById('subscribe-btn').onclick = function(){
     if(! isEmail(document.getElementById('email-input').value)){
-
+        document.getElementById('wrong-email-text').style.display = 'block';
+        document.getElementById('email-input').style.border = '1px solid hsl(4, 100%, 67%)';
+        document.getElementById('email-input').style.color = 'hsl(4, 100%, 67%)';
+        document.getElementById('email-input').style.backgroundColor = 'hsl(4, 100%, 97%)'
     }else{
-        
+
     }
 }
